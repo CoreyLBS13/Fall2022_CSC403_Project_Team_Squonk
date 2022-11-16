f2b1a62c11988619a494da28add2d03cd6e72110
CoreyLBS13 <Coreyleebelk@icloud.com>
2022-11-16 14:05:49 -0600

    Adjustment to help menu

--- Project/Fall2020_CSC403_Project/FrmHelp.Designer.cs
+++ Project/Fall2020_CSC403_Project/FrmHelp.Designer.cs
@@ -59,9 +59,10 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("MV Boli", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(268, 33);
+            this.label1.Location = new System.Drawing.Point(715, 79);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 39);
+            this.label1.Size = new System.Drawing.Size(184, 96);
             this.label1.TabIndex = 0;
             this.label1.Text = "Goal";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -71,9 +72,10 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(63, 72);
+            this.label2.Location = new System.Drawing.Point(168, 172);
+            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(474, 75);
+            this.label2.Size = new System.Drawing.Size(1131, 189);
             this.label2.TabIndex = 1;
             this.label2.Text = "Work your way through the map, beating each\r\nenemy one by one until you finally r" +
     "each and\r\nsave Squonk!";
@@ -84,9 +86,10 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("MV Boli", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(231, 157);
+            this.label3.Location = new System.Drawing.Point(616, 374);
+            this.label3.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(134, 39);
+            this.label3.Size = new System.Drawing.Size(324, 96);
             this.label3.TabIndex = 2;
             this.label3.Text = "Controls";
             // 
@@ -95,9 +98,10 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.keys;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(61, 196);
+            this.pictureBox1.Location = new System.Drawing.Point(163, 467);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(164, 91);
+            this.pictureBox1.Size = new System.Drawing.Size(437, 217);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
@@ -106,9 +110,10 @@
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.ctrl;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox2.Location = new System.Drawing.Point(61, 293);
+            this.pictureBox2.Location = new System.Drawing.Point(163, 699);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(164, 97);
+            this.pictureBox2.Size = new System.Drawing.Size(437, 231);
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
             // 
@@ -117,9 +122,10 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(233, 219);
+            this.label4.Location = new System.Drawing.Point(621, 522);
+            this.label4.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(291, 50);
+            this.label4.Size = new System.Drawing.Size(699, 126);
             this.label4.TabIndex = 5;
             this.label4.Text = "Use the arrow keys to move\r\naround the map";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -129,11 +135,12 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(232, 305);
+            this.label5.Location = new System.Drawing.Point(619, 727);
+            this.label5.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(305, 75);
+            this.label5.Size = new System.Drawing.Size(712, 189);
             this.label5.TabIndex = 6;
-            this.label5.Text = "Hold ctrl to crouch when you\r\nneed to get through tight \r\nand dangerous spaces ";
+            this.label5.Text = "Hold ctrl to shrink when you\r\nneed to get through tight \r\nand dangerous spaces ";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // pictureBox3
@@ -141,9 +148,10 @@
             this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox3.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.player;
             this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox3.Location = new System.Drawing.Point(605, 72);
+            this.pictureBox3.Location = new System.Drawing.Point(1613, 172);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(90, 144);
+            this.pictureBox3.Size = new System.Drawing.Size(240, 343);
             this.pictureBox3.TabIndex = 7;
             this.pictureBox3.TabStop = false;
             // 
@@ -152,9 +160,10 @@
             this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox4.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.Mrs;
             this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox4.Location = new System.Drawing.Point(1036, 209);
+            this.pictureBox4.Location = new System.Drawing.Point(2763, 498);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(129, 171);
+            this.pictureBox4.Size = new System.Drawing.Size(344, 408);
             this.pictureBox4.TabIndex = 8;
             this.pictureBox4.TabStop = false;
             // 
@@ -163,9 +172,10 @@
             this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox5.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.baby;
             this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox5.Location = new System.Drawing.Point(605, 396);
+            this.pictureBox5.Location = new System.Drawing.Point(1613, 944);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(58, 99);
+            this.pictureBox5.Size = new System.Drawing.Size(155, 236);
             this.pictureBox5.TabIndex = 9;
             this.pictureBox5.TabStop = false;
             // 
@@ -174,9 +184,10 @@
             this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox6.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.p;
             this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox6.Location = new System.Drawing.Point(68, 396);
+            this.pictureBox6.Location = new System.Drawing.Point(181, 944);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(151, 111);
+            this.pictureBox6.Size = new System.Drawing.Size(403, 265);
             this.pictureBox6.TabIndex = 10;
             this.pictureBox6.TabStop = false;
             // 
@@ -185,9 +196,10 @@
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(232, 429);
+            this.label6.Location = new System.Drawing.Point(619, 1023);
+            this.label6.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(303, 50);
+            this.label6.Size = new System.Drawing.Size(718, 126);
             this.label6.TabIndex = 11;
             this.label6.Text = "Press \"P\" during the level to\r\npause the game";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -197,9 +209,10 @@
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("MV Boli", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(795, 33);
+            this.label7.Location = new System.Drawing.Point(2120, 79);
+            this.label7.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(165, 39);
+            this.label7.Size = new System.Drawing.Size(400, 96);
             this.label7.TabIndex = 12;
             this.label7.Text = "Characters";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -209,9 +222,10 @@
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(701, 81);
+            this.label8.Location = new System.Drawing.Point(1869, 193);
+            this.label8.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(442, 125);
+            this.label8.Size = new System.Drawing.Size(1048, 315);
             this.label8.TabIndex = 13;
             this.label8.Text = "Mr. Peanut is the patriarch of the Peanut\r\nfamily and uses his superior strength\r" +
     "\nto fight. His special attack uses his cane\r\nto enhance his damage but the cane " +
@@ -223,9 +237,10 @@
             this.label9.AutoSize = true;
             this.label9.BackColor = System.Drawing.Color.Transparent;
             this.label9.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(600, 219);
+            this.label9.Location = new System.Drawing.Point(1600, 522);
+            this.label9.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(459, 150);
+            this.label9.Size = new System.Drawing.Size(1090, 378);
             this.label9.TabIndex = 14;
             this.label9.Text = resources.GetString("label9.Text");
             this.label9.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -235,9 +250,10 @@
             this.label10.AutoSize = true;
             this.label10.BackColor = System.Drawing.Color.Transparent;
             this.label10.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(669, 383);
+            this.label10.Location = new System.Drawing.Point(1784, 913);
+            this.label10.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(480, 125);
+            this.label10.Size = new System.Drawing.Size(1132, 315);
             this.label10.TabIndex = 15;
             this.label10.Text = resources.GetString("label10.Text");
             this.label10.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -246,9 +262,10 @@
             // 
             this.closeBtn.BackColor = System.Drawing.Color.IndianRed;
             this.closeBtn.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.closeBtn.Location = new System.Drawing.Point(505, 530);
+            this.closeBtn.Location = new System.Drawing.Point(1347, 1264);
+            this.closeBtn.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.closeBtn.Name = "closeBtn";
-            this.closeBtn.Size = new System.Drawing.Size(143, 54);
+            this.closeBtn.Size = new System.Drawing.Size(381, 129);
             this.closeBtn.TabIndex = 16;
             this.closeBtn.Text = "Close";
             this.closeBtn.UseVisualStyleBackColor = false;
@@ -256,11 +273,11 @@
             // 
             // FrmHelp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.helpScreen;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1184, 601);
+            this.ClientSize = new System.Drawing.Size(3157, 1433);
             this.Controls.Add(this.closeBtn);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
@@ -279,6 +296,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "FrmHelp";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Help";
